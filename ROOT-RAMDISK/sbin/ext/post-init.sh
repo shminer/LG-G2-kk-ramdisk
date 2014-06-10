d8f1e731e4de72a62326d76bf59da2f305c7b851
shminer <332574643@qq.com>
2014-06-10 09:25:27 +0800

    change defaule gpu devfreq

--- ROOT-RAMDISK/sbin/ext/post-init.sh
+++ ROOT-RAMDISK/sbin/ext/post-init.sh
@@ -129,7 +129,7 @@ $BB chmod 666 /sys/class/kgsl/kgsl-3d0/max_gpuclk
 $BB chmod 666 /sys/devices/fdb00000.qcom,kgsl-3d0/devfreq/fdb00000.qcom,kgsl-3d0/governor
 
 # make sure our max gpu clock is set via sysfs
-echo "450000000" > /sys/devices/fdb00000.qcom,kgsl-3d0/devfreq/fdb00000.qcom,kgsl-3d0/max_freq
+echo "578000000" > /sys/devices/fdb00000.qcom,kgsl-3d0/devfreq/fdb00000.qcom,kgsl-3d0/max_freq
 
 # set min max boot freq to default.
 echo "2803200" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
